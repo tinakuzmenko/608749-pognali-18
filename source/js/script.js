08cad30af7c9a35c467c254e31067367feaa7f53
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2019-11-30 22:47:09 +0100

    Добавила смену цвета навигации при скролле

--- source/js/script.js
+++ source/js/script.js
@@ -2,14 +2,21 @@ var nav = document.querySelector(".page-nav");
 var navToggle = document.querySelector(".page-nav__toggler");
 var headerContent = document.querySelector(".page-header__content-wrapper");
 
-/* Убираем &--no-js */
 nav.classList.remove("page-nav--no-js");
 headerContent.classList.remove("page-header__content-wrapper--no-js");
-
-/* Раскрытие и закрытие навигации */
 
 navToggle.addEventListener("click", function(){
   nav.classList.toggle("page-nav--active");
   nav.classList.toggle("page-nav--inactive");
-  nav.classList.toggle("page-nav--white");
+  nav.classList.add("page-nav--white");
+  if(nav.classList.contains("page-nav--inactive")){
+    nav.classList.remove("page-nav--white");
+  }
+});
+
+window.addEventListener("scroll", function(){
+  nav.classList.add("page-nav--white");
+  if(pageYOffset === 0 && nav.classList.contains("page-nav--inactive")) {
+    nav.classList.remove("page-nav--white");
+  }
 });
