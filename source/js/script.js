0d52d4cd6c9fc6a357f9aa86a2b4bbbe0c8a9422
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2019-11-30 20:33:31 +0100

    Реализовала стили и функционал навигации в различных состояниях

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,13 @@
 var nav = document.querySelector(".page-nav");
 var navToggle = document.querySelector(".page-nav__toggler");
 
+/* Убираем &--no-js */
+nav.classList.remove("page-nav--no-js");
+
 /* Раскрытие и закрытие навигации */
 
 navToggle.addEventListener("click", function(){
-  nav.classList.toggle("page-nav--inactive");
   nav.classList.toggle("page-nav--active");
+  nav.classList.toggle("page-nav--inactive");
+  nav.classList.toggle("page-nav--white");
 });
