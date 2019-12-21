b8c857d111986c665a2f28e11b4d98b8d41d32d0
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2019-12-21 22:34:45 +0100

    Закончила стилизацию поп-апа в форме

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -2,13 +2,15 @@ var accordion = document.querySelector(".accordion");
 var accordionButtons = accordion.querySelectorAll(".accordion__item");
 
 for (var i = 0; i < accordionButtons.length; i++) {
-  accordionButtons[i].addEventListener("click", function() {
-    var title = this.querySelector(".accordion__title");
-    title.classList.toggle("accordion__title--collapse");
+  if (document.documentElement.clientWidth < 768 || document.documentElement.clientWidth >= 1440) {
+    accordionButtons[i].addEventListener("click", function() {
+      var title = this.querySelector(".accordion__title");
+      title.classList.toggle("accordion__title--collapse");
 
-    var accordionList = this.nextElementSibling;
-    accordionList.classList.toggle("accordion__list--hidden");
+      var accordionList = this.nextElementSibling;
+      accordionList.classList.toggle("accordion__list--hidden");
 
-    console.log(this)
-  });
+      console.log(this)
+    });
+  }
 }
--- source/js/chose-country.js
+++ source/js/chose-country.js
@@ -4,14 +4,19 @@ var countrySelectPopup = document.querySelector(".chose-country");
 
 countrySelectButton.addEventListener("click", function(){
   countrySelectPopup.classList.toggle("chose-country--active");
-  countryDeleteButton.classList.toggle("plan-step__delete-country--inactive");
   countrySelectButton.classList.toggle("country-select--blue");
+  if (document.documentElement.clientWidth < 768) {
+    countryDeleteButton.classList.toggle("plan-step__delete-country--inactive");
+  }
 });
 
 window.addEventListener("keydown", function(event){
   if(event.keyCode === 27) {
     countrySelectPopup.classList.remove("chose-country--active");
-    countryDeleteButton.classList.remove("plan-step__delete-country--inactive");
     countrySelectButton.classList.remove("country-select--blue");
+
+    if (document.documentElement.clientWidth < 768) {
+      countryDeleteButton.classList.remove("plan-step__delete-country--inactive");
+    }
   }
 });
