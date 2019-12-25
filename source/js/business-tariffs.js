bca17be2025907eb68083dc21594c265c3ec858f
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2019-12-25 22:24:14 +0100

    Добавила состояния для некоторых интерактивных элементов

--- source/js/business-tariffs.js
+++ source/js/business-tariffs.js
@@ -1,22 +1,18 @@
 var businessPopupOpen = document.querySelector(".add-profile__business-link");
 var businessPopup = document.querySelector(".business-popup");
 var businessPopupClose = document.querySelector(".business-popup__close-button");
-var pageBody = document.querySelector(".page");
 
 businessPopupOpen.addEventListener("click", function(event){
   event.preventDefault();
   businessPopup.classList.add("business-popup--active");
-  pageBody.classList.add("page--no-scroll");
 });
 
 businessPopupClose.addEventListener("click", function(){
   businessPopup.classList.remove("business-popup--active");
-  pageBody.classList.remove("page--no-scroll");
 });
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     businessPopup.classList.remove("business-popup--active");
-    pageBody.classList.remove("page--no-scroll");
   }
 });
