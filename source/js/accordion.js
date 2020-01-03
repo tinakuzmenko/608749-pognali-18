b1ea83b032d976edf5b19d4c6aaa4c0caf248a89
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-01-03 20:38:51 +0100

    Закончила добавление ретиновой графики на страницы, добавила автоматическое скрытие аккордеона при включенном JS

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,16 +1,26 @@
 var accordion = document.querySelector(".accordion");
 var accordionButtons = accordion.querySelectorAll(".accordion__item");
+var accordionLevel = accordion.querySelector(".accordion__level-wrapper");
+var accordionAllLists = accordion.querySelectorAll(".accordion__list");
 
 for (var i = 0; i < accordionButtons.length; i++) {
-  if (document.documentElement.clientWidth < 768 || document.documentElement.clientWidth >= 1440) {
+  if (
+    document.documentElement.clientWidth < 768 ||
+    document.documentElement.clientWidth >= 1440
+  ) {
+    accordionLevel.classList.add("accordion__level-wrapper--hidden");
+    for (var j = 0; j < accordionAllLists.length; j++) {
+      accordionAllLists[j].classList.add("accordion__list--hidden");
+    }
     accordionButtons[i].addEventListener("click", function() {
       var title = this.querySelector(".accordion__title");
       title.classList.toggle("accordion__title--collapse");
-
       var accordionList = this.nextElementSibling;
-      accordionList.classList.toggle("accordion__list--hidden");
-
-      console.log(this)
+      if (accordionList === accordionLevel) {
+        accordionLevel.classList.toggle("accordion__level-wrapper--hidden");
+      } else {
+        accordionList.classList.toggle("accordion__list--hidden");
+      }
     });
   }
 }
