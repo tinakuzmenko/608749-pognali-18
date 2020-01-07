0ea779b18d2a939fa27c6df6310ddf91637c7c08
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-01-07 17:38:50 +0100

    Исправила проект по результатам замечаний проверяющего наставника

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -5,20 +5,20 @@ var headerContent = document.querySelector(".page-header__content-wrapper");
 nav.classList.remove("page-nav--no-js");
 headerContent.classList.remove("page-header__content-wrapper--no-js");
 
-navToggle.addEventListener("click", function(){
+navToggle.addEventListener("click", function() {
   nav.classList.toggle("page-nav--active");
   nav.classList.toggle("page-nav--inactive");
   nav.classList.add("page-nav--white");
-  if(pageYOffset === 0 && nav.classList.contains("page-nav--inactive")){
+  if (pageYOffset === 0 && nav.classList.contains("page-nav--inactive")) {
     nav.classList.remove("page-nav--white");
   }
 });
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
   nav.classList.add("page-nav--white");
   nav.classList.add("page-nav--scrolled");
   nav.classList.remove(".page-nav--inner");
-  if(pageYOffset === 0 && nav.classList.contains("page-nav--inactive")) {
+  if (pageYOffset === 0 && nav.classList.contains("page-nav--inactive")) {
     nav.classList.remove("page-nav--white");
     nav.classList.remove("page-nav--scrolled");
     nav.classList.add("page-nav--inner");
